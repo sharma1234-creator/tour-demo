cd9c8c4f4a99975bd2274b07ab3b2b48b7a8b573
sharma1234-creator <sharmaabhishekkumar561@gmail.com>
2021-09-12 23:34:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header/>
+      <Content/>
+      <Footer/>
+    </React.Fragment>
+  );
+}
+
+export default App;
